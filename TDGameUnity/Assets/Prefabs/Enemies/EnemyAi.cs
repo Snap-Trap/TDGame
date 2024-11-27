01a63612ef4f2f95f24c68d541c888cef359429a
Snap-Trap <111644953+Snap-Trap@users.noreply.github.com>
2024-11-27 12:09:46 +0100

    Enemy can die when reaching the goal
    It's with a tag, so you HAVE to use the prefab, I also added an enemy destination in said prefab for the enemy to move towards, so it will automatically go towards the End if you connect it to a path

--- TDGameUnity/Assets/Prefabs/Enemies/EnemyAi.cs
+++ TDGameUnity/Assets/Prefabs/Enemies/EnemyAi.cs
@@ -41,4 +41,19 @@ public class EnemyAi : MonoBehaviour
         walkPoint = new Vector3(enemyDestination.x, enemyDestination.y, enemyDestination.z);
         walkpointSet = true;
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("EndOfPath"))
+        {
+           StartCoroutine(WaitBeforeDestroy());
+        }
+    }
+
+    IEnumerator WaitBeforeDestroy()
+    {
+        yield return new WaitForSeconds(1);
+        Destroy(gameObject);
+        Debug.Log("Enemy reached the end of the line");
+    }
 }
