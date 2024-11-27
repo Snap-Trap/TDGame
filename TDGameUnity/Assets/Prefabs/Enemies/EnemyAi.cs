28e1833a7b55c7395c4e1bddece5faeadff7b260
Snap-Trap <111644953+Snap-Trap@users.noreply.github.com>
2024-11-27 09:59:51 +0100

    Finished basic enemy ai

--- TDGameUnity/Assets/Prefabs/Enemies/EnemyAi.cs
+++ TDGameUnity/Assets/Prefabs/Enemies/EnemyAi.cs
@@ -7,19 +7,38 @@ public class EnemyAi : MonoBehaviour
 {
     public float speed, health;
 
-    public LayerMask whatIsPath, whatIsEndOfPath;
+    private Vector3 walkPoint, enemyDestination;
+
+    [SerializeField] private bool walkpointSet;
+
+    private LayerMask whatIsPath;
     
     private NavMeshAgent agent;
 
     private void Start()
     {
+        whatIsPath = LayerMask.GetMask("pathLayer");
+        enemyDestination = GameObject.Find("EnemyDestination").transform.position;
         agent = GetComponent<NavMeshAgent>();
         agent.speed = speed;
     }
 
     private void Update()
     {
-        agent.SetDestination(FindPath());
+        if (!walkpointSet)
+        {
+            SearchWalkPoint();
+        }   
 
+        else
+        {
+            agent.SetDestination(walkPoint);
+        }
+    }
+
+    private void SearchWalkPoint()
+    {
+        walkPoint = new Vector3(enemyDestination.x, enemyDestination.y, enemyDestination.z);
+        walkpointSet = true;
     }
 }
