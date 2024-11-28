c34615d33f86379cac519b24a2b865c6c4c5efb2
Poeloe <jmpul813@gmail.com>
2024-11-28 10:49:54 +0100

    copy pase luca naar mij

---
+++ TDGameUnity/Assets/Prefabs/enemy'sM/placeholdersM/EnemyAi2.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAi2 : MonoBehaviour, IDamageable
+{
+    EnemyStats enemyStats;
+
+    private float distanceTraveled;
+
+    private Vector3 walkPoint, enemyDestination;
+
+    [SerializeField] private bool walkpointSet;
+
+    private LayerMask whatIsPath;
+
+    private NavMeshAgent agent;
+
+    //---------------------
+    private waveSpawn wavespawn;
+
+
+    private void Start()
+    {
+        whatIsPath = LayerMask.GetMask("pathLayer");
+        enemyDestination = GameObject.Find("EnemyDestination").transform.position;
+        agent = GetComponent<NavMeshAgent>();
+        //---------------------
+        wavespawn = GetComponentInParent<waveSpawn>();
+
+    }
+
+    private void Update()
+    {
+        distanceTraveled += agent.velocity.magnitude * Time.deltaTime;
+
+        Debug.Log(distanceTraveled);
+
+        if (!walkpointSet)
+        {
+            SearchWalkPoint();
+        }
+
+        else
+        {
+            agent.SetDestination(walkPoint);
+        }
+    }
+
+    private void SearchWalkPoint()
+    {
+        walkPoint = new Vector3(enemyDestination.x, enemyDestination.y, enemyDestination.z);
+        walkpointSet = true;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("EndOfPath"))
+        {
+            StartCoroutine(WaitBeforeDestroy());
+        }
+    }
+
+    public void TakeDamage(float damage)
+    {
+        enemyStats.health -= damage;
+        if (enemyStats.health <= 0)
+        {
+            Destroy(gameObject);
+            Debug.Log("THY END IS NOW!!!");
+            //---------------------
+            wavespawn.waves[wavespawn.currentWave].enemiesCount--;
+        }
+    }
+
+    IEnumerator WaitBeforeDestroy()
+    {
+        yield return new WaitForSeconds(1);
+        Destroy(gameObject);
+        Debug.Log("Enemy reached the end of the line");
+        //---------------------
+        wavespawn.waves[wavespawn.currentWave].enemiesCount--;
+    }
+}
