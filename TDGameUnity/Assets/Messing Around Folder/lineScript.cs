8bc19c96d598f76835ebc058f998e3f4f076589e
luckyman9000 <110716744+luckyman9000@users.noreply.github.com>
2024-11-29 11:41:56 +0100

    asdfghjkl;'

--- TDGameUnity/Assets/Messing Around Folder/lineScript.cs
+++ TDGameUnity/Assets/Messing Around Folder/lineScript.cs
@@ -7,22 +7,29 @@ using UnityEngine;
 public class lineScript : MonoBehaviour
 {
     private Vector3[] Positions;
+    private float t;
     // Start is called before the first frame update
     void Start()
     {
-        
+        GetComponent<LineRenderer>().SetWidth(0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        Positions = new Vector3[] { new Vector3(0, 0, 0), new Vector3(transform.position.x, transform.position.y, transform.position.z) };
+        Positions = new Vector3[] { new Vector3(0, 0, 0), transform.position };
 
         GetComponent<LineRenderer>().SetPositions(Positions);
 
+        if (Input.GetMouseButton(0))
+        {
 
-        
+            GetComponent<LineRenderer>().SetWidth(t * 100, t * 100);
+            t += Time.deltaTime;
+
+        }
+
     }
-
-    private void Fire()
 }
+
+
