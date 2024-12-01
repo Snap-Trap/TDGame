a83cbc7f37e88b271907498030b7fc30c48ca4fe
Poeloe <jmpul813@gmail.com>
2024-12-01 20:30:13 +0100

    coins

---
+++ TDGameUnity/Assets/extraScripts/towerButtons.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class towerButtons : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Tower Buttons Script is working");
+    }
+
+    void Update()
+    {
+        
+    }
+}
